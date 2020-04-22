fe7f6d05e7ea2d9510b975896f2efe56d078e09b
black xie <blackxie@outlook.com>
2020-04-22 14:43:05 +0800

    初始版本

--- components/my-event/my-event.js
+++
@@ -1,9 +0,0 @@
-// components/my-event/my-event.js
-Component({
-  methods: {
-    handleIncrement() {
-      // console.log(1111111111);
-      this.triggerEvent('increment', {}, {})
-    }
-  }
-})
